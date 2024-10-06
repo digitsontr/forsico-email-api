ebeeffc7f7fc426b146290609e3192523e3d1500
Furkan Yıldız <93140144+FyPap@users.noreply.github.com>
2024-10-06 18:48:36 +0300

    env parameter added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mailRoutes = require('./routes/mailRoutes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use('/api/mail', mailRoutes);
