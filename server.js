165782f52b6df1ea74cabefe41c44c302dbecb42
Furkan Yıldız <93140144+FyPap@users.noreply.github.com>
2024-10-06 17:08:32 +0300

    routes updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 //const bodyParser = require('body-parser');
 const mailRoutes = require('./routes/mailRoutes');
-const { sequelize } = require('./models');
+//const { sequelize } = require('./models');
 
 const app = express();
 const port = 3000;
@@ -10,15 +10,8 @@ app.use(express.json());
 //app.use(bodyParser.json());
 app.use('/api/mail', mailRoutes);
 
-// Veritabanı tablolarını oluştur
-sequelize.sync({ force: false })  // force: true dikkatli kullanın, mevcut verileri siler!
-    .then(() => {
-        console.log('Database synced');
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
-    })
-    .catch(err => {
-        console.error('Unable to sync database:', err);
-    });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
