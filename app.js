edb0ee5d58814cfc900956107fef6aebeecc1fce
Furkan Yıldız <93140144+FyPap@users.noreply.github.com>
2024-11-02 14:47:04 +0300

    first deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mailRoutes = require('./routes/mailRoutes');
 const app = express();
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use('/api/mail', mailRoutes);
